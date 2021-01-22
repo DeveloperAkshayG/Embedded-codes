8e2ac381a5fdc148e9568de976270238cea86d38
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2021-01-22 15:03:43 +0530

    updated code of timer1 and interrupt
    flowmeter version2

--- ft.c
+++ ft.c
@@ -1,4 +1,4 @@
-//flow meter using timer1 without prescalar and 5ms delay
+//flow meter using timer1 without prescalar and 5ms delay 
 #define F_CPU 8000000UL
 #include<avr/io.h>
 #include<util/delay.h>
@@ -14,7 +14,7 @@
 #define RW PE3
 
 float ticks=0.00;
-unsigned long overflow=0;
+volatile unsigned long overflow;
 unsigned int tcntres=0;
 double volume=0.00;       //for volume calulation
 double flow=0.00;		   //for flow calculation flow=vol*60/sec
@@ -22,14 +22,16 @@ char tcntresult[20]={0};
 char overresult[20]={0};
 char ticksres[20]={0};
 char flowres[20]={0};
+int flag0;
 
 ISR(INT0_vect)
 {
 	PORTE=0b00000000;     //led off
 	TCCR1B |= (1<<CS10);   //no prescalar and normal mode
-	EIMSK= 0b00000110;      //setting interrupt1 on and interrupt0 off
-	EICRA=0X0C;              //setting interrupt1 for rising edge
-//	sei();
+	EIFR=  0b00000010;    //INT1 DISABLED SO TO RENABLED USE IT
+	EIMSK= 0b00000110;      //setting interrupt1 on and interrupt0 off uncomment after
+	EICRA=0X0C;              //setting interrupt1 for rising edge uncomment after
+	sei();
 //	while(1)
 //	{
 //		
@@ -44,30 +46,33 @@ ISR(INT1_vect)
 	TCCR1B=0X00;
 //	ticks=(((float)overflow) * 0.009984) + (((float)tcntres) * 0.000128);  //for 1024 prescalar with calculation changes
 	
-	ticks=(((float)overflow) * 0.005) + (((float)tcntres) * 0.000000125);  //for 1024 prescalar with calculation changes
+	ticks=(((float)overflow) * 0.005) + (((float)tcntres) * 0.000000125);  //for 1024 prescalar using timer1
 
 	//sprintf(ticksres,"t=%.2f",ticks);
 	//LCD_String_xy(0,0,ticksres);
 	
-	flow=((volume*60)/ticks);
-	sprintf(flowres,"t=%.2f fl=%lf",ticks,flow);
+	//flow=((volume*60)/ticks);
+	flow=((volume)/ticks);           //on 21 jan
+	sprintf(flowres,"t=%.3f fl=%lf",ticks,flow);  //changed from .2f to .3f on 21 jan
 	LCD_String_xy(0,0,flowres);
-	sprintf(tcntresult,"tcnt=%u ov=%lu  ",tcntres,overflow);
+	//sprintf(tcntresult,"tcnt=%u ov=%lu  ",tcntres,overflow);
+	sprintf(tcntresult,"ov=%lu vo=%lf  ",overflow,volume);
+	//LCD_String_xy(1,0,tcntresult);
 	LCD_String_xy(1,0,tcntresult);
+	memset(flowres,0,20);
+	memset(tcntresult,0,20);
 	ticks=0.00;
 	tcntres=0;
 	overflow=0;
 	flow=0.00;
-	memset(flowres,0,20);
-	memset(tcntresult,0,20);
 	EIMSK = 0b00000100;
 }
 
 ISR(INT2_vect)
 {
+	EIMSK = 0b00000001;
 	LCD_Clear();
-	PORTE=~PORTE;		/* Toggle PORTE */
-	_delay_ms(100); 
+//	PORTE=~PORTE;		// Toggle PORTE  
 	TCCR1B=0X00;
 	memset(flowres,0,20);
 	memset(tcntresult,0,20);
@@ -78,8 +83,11 @@ ISR(INT2_vect)
 //	LCD_Clear();
 	LCD_String_xy(0,1,"WELCOME TO");
 	LCD_String_xy(1,1,"NETEL FLOWMETER");
+	EIFR=  0b00000100; 
 	EIMSK = 0b00000101;
+	sei();
 }
+
 
 
 ISR (TIMER1_OVF_vect)  //TIMER OVERFLOW ISR
@@ -93,6 +101,7 @@ void timer1_init()
 	TCNT1=25536;     //from calculation 
 	overflow=0;
 	TIMSK |= (1<<TOIE1);    //INTERRUPT FOR TIMER1 OVERFLOW
+	sei();
 }
 
 void LCD_Command(unsigned char cmnd)
@@ -173,28 +182,35 @@ int main ( )
 	
     LCD_Init();  //intialize lcd
 	
-	float ID=1.7;             //diameter=3.4mm so radius of capillary in mm is d/2 which is 1.7mm
-	int height=47;	   //height of capillary in mm
-	double pie=3.14285714286; //pie value used for volume calculation
+	//float ID=1.7;             //diameter=3.3mm so radius of capillary in mm is d/2 which is 1.65mm
+	float ID=4;             //diameter=3.3mm so radius of capillary in mm is d/2 which is 1.65mm
+	//int height=47;	   //height of capillary in mm
+	int height=50;	   //height of capillary in mm
+	double pie=3.14159265358; //pie value used for volume calculation
 	
 	LCD_String_xy(0,0,"WELCOME TO");
 	LCD_String_xy(1,0,"NETEL FLOWMETER");
 	_delay_ms(1000);
-	volume=(pie*(ID)*(ID)*height)/1000;   //volume in mm3 which is converted into ml by dividing it with 1000
+//	volume=(pie*(ID)*(ID)*height)/1000;   //volume in mm3 which is converted into ml by dividing it with 1000
+	
+	volume=((pie*(ID)*(ID)*height)/1000)*60;   //volume in mm3 which is converted into ml by dividing it with 1000 on 21 jan
 	
 //	EIMSK |= (1<<INT0); //set int0 interrupt
-	EIMSK = 0b00000101; //set int0 and int2 interrupt
+	EIMSK = 0b00000101; //set int0 and int2 interrupt 
 //	EICRA=0b00100011;  //INT0 CONFIGURED FOR rising EDGE AND INT2 FOR FALLING EDGE
-	
-	EICRA=0b00000011;  //INT0 CONFIGURED FOR rising EDGE AND INT2 FOR LOW LEVEL
+	EIMSK &= ~(1<<INT1);
+	EICRA=0b00000011;  //INT0 CONFIGURED FOR rising EDGE AND INT2 FOR LOW LEVEL 
+ 
 
+	
 	sei();
 	timer1_init();
 	LCD_Clear();
 	while(1)
 	{
 		//DO NOTHING
-	//	_delay_ms(1000);
+		_delay_ms(1000);
+		PORTE=~PORTE;
 		
 	}
 }
