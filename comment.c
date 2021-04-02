4bc3a63eac52cfd4dba817dd622facfb6ceef9b2
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2021-04-02 12:08:51 +0530

    Update comment.c

--- comment.c
+++ comment.c
@@ -1,10 +1,11 @@
 ##THIS CODE HELPS IN UNDERSTANDING THE #if0 and #endif directive which helps to comment out multiple sections of code you can also use #if0 instead of #if(i>1)
-
+##but if you use #define i 5 and execute it all will be printed
 #include <stdio.h>
 int main()
 {
     int i=5;
     #if(i>1) 
+  //#if 0  
     printf("last\n");
     printf("pop\n");
     printf("push\n");
